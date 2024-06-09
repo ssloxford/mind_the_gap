c12af1677d01df2a42db31dcd0af39585e37dbfc
Sapino <isis.daude@epfl.ch>
2024-06-09 13:02:53 +0100

    Packet analysis

--- experiments/packet_analysis.py
+++ experiments/packet_analysis.py
@@ -2,7 +2,7 @@ from sniffing.tools import statistics_capture_for_distance
 import pprint
 
 # Statistics capture for each distance
-distances = [1, 2, 3, 4, 5]
+distances = [1, 2, ]#3, 4, 5]
 distance_data = dict()
 for i in range(len(distances)):
     distance_data = statistics_capture_for_distance(distances[i], distance_data, 10, "simple_setup_exp/passive/wireless/c3m","c3m_a1m")
